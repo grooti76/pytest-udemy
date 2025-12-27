205160c6142bc22106054f47384f8ddc57da0788
Shiv Kumar Jha <86116769+grooti76@users.noreply.github.com>
2025-12-27 17:14:04 +0000

    2nd section completed

---
+++ main.py
@@ -0,0 +1,15 @@
+def test_a1():
+    assert 5+5==10
+    assert 5-5==0
+    assert 5+10==15
+
+
+
+# this is a failed test
+def test_a2():
+    assert 9/5==1
+
+
+def test_a3():
+    assert 9//5==1
+
